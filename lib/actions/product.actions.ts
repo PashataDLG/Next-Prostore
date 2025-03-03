13f7f9cd51aab1d1b0cdaae00a7eb362b7bccc10
Angel <angel.pashov@proton.me>
2025-03-03 19:21:27 +0200

    getLatestProducts now return price as a string instead of Decimal type

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,5 @@
 "use server";
 import { PrismaClient } from "@prisma/client";
-import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
@@ -12,5 +11,9 @@ export async function getLatestProducts() {
         orderBy: { createdAt: "desc" },
     });
 
-    return convertToPlainObject(data);
+    return data.map((product) => ({
+        ...product,
+        price: product.price.toString(),
+        rating: product.rating.toString(),
+      }));
 }
