6527f43085ce6e1b5d27634cfcde1f4c3e89ff40
Angel <angel.pashov@proton.me>
2025-03-03 17:12:12 +0200

    product list now renders the product card

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import ProductCard from "./product-card";
+
 const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?:number }) => {
     const limitedData = limit ? data.slice(0, limit) : data;
 
@@ -8,7 +10,7 @@ const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?
             {data.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {limitedData.map((product: any) => (
-                        <div key={product.slug}>{product.name}</div>
+                        <ProductCard key={product.slug} product={product}/>
                     ))}
                 </div>
             ) : (
