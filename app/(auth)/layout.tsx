b25e5778b43e19a8780d36057a4a9eec07ff3fb8
Angel <angel.pashov@proton.me>
2025-03-07 18:08:50 +0200

    auth layout added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex-center min-h-screen w-full">
+            <main className="flex-1 wrapper">
+                {children}
+            </main>
+        </div>
+    );
+}
