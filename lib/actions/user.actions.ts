78103c4cb8e33f71fb621a027de24e698c098c16
Angel <angel.pashov@proton.me>
2025-03-10 16:12:20 +0200

    created signUpUser action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 "use server";
 
-import { signInFromSchema } from "../validators";
+import { signInFromSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 // Sign in user with credentials
 export async function signInWithCredentials(
@@ -30,4 +32,41 @@ export async function signInWithCredentials(
 //Sign User Out
 export async function signOutUser() {
     await signOut();
+}
+
+// Sign Up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+    try {
+        const user = signUpFormSchema.parse({
+            name: formData.get("name"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+            confirmPassword: formData.get("confirmPassword"),
+        });
+
+        const plainPassword = user.password;
+
+        user.password = hashSync(user.password, 10);
+
+        await prisma.user.create({
+            data: {
+                name: user.name,
+                email: user.email,
+                password: user.password,
+            },
+        });
+
+        await signIn("credentials", {
+            email: user.email,
+            password: plainPassword,
+        });
+
+        return { success: true, message: "User registered successfully" };
+    } catch (error) {
+        if (isRedirectError(error)) {
+            throw error;
+        }
+    }
+
+    return { successful: false, message: "User registration failed" };
 }
